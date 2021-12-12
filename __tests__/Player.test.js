b8bd6a6d47d0e5ff714d545cd9b109750c4a641d
Henry <henryolson@icloud.com>
2021-12-12 15:12:36 -0600

    Refactored the the Player object functionality

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,11 +1,7 @@
-const Potion = require('../lib/potion');
+const Player = require('../lib/player.js')
+const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-
-console.log(new Potion());
-
-const Player = require('../lib/Player');
-const { expect } = require('@jest/globals');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -19,3 +15,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
